0fbd01fbbe251333e121fc8bf3385ef9a32efea6
Ejaz Ali <ejazali4@protonmail.ch>
2021-09-06 13:39:10 +0100

    ✨ Camel-case of icons + more icons

---
+++ src/icons/SecureFilled12/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SecureFilled12 = (args: any) => {
+    return (
+		<svg width="12" height="12" viewBox="0 0 12 12" {...args}>
+			<rect x="1" y="4" width="10" height="8" rx="2"></rect>
+			<rect x="3.5" y="0.5" width="5" height="7" rx="2.5"></rect>
+		</svg>
+    )
+}
+
+export default SecureFilled12;
--- src/manifest.ts
+++ src/manifest.ts
@@ -1,49 +1,69 @@
-import AddPrivateWindow from './icons/AddPrivateWindow';
-import AddPrivateWindow1 from './icons/AddPrivateWindow1';
-import AddWindow from './icons/AddWindow';
+import AddPrivate from './icons/AddPrivate';
+import AddPrivate1 from './icons/AddPrivate1';
+import Add from './icons/Add';
+import Add1 from './icons/Add1';
 import All from './icons/All';
-import Audio from './icons/Audio';
+import AppearanceFilled from './icons/AppearanceFilled';
 import AudioNone from './icons/AudioNone';
+import Audio from './icons/Audio';
+import AvatarFilled from './icons/AvatarFilled';
+import Avatar from './icons/Avatar';
+import Back from './icons/Back';
 import Biscuit from './icons/Biscuit';
 import Bold from './icons/Bold';
 import BulletList from './icons/BulletList';
 import Calculator from './icons/Calculator';
 import Calculator2 from './icons/Calculator2';
 import Certificate from './icons/Certificate';
+import Check from './icons/Check';
 import ChevronDown from './icons/ChevronDown';
 import ChevronLeft from './icons/ChevronLeft';
 import ChevronRight from './icons/ChevronRight';
 import ChevronUp from './icons/ChevronUp';
 import Clock from './icons/Clock';
-import CloseTabs from './icons/CloseTabs';
 import CloseTabsToLeft from './icons/CloseTabsToLeft';
 import CloseTabsToRight from './icons/CloseTabsToRight';
+import CloseTabs from './icons/CloseTabs';
+import Close from './icons/Close';
 import Colour from './icons/Colour';
+import CompassFilled from './icons/CompassFilled';
 import Copy from './icons/Copy';
 import Cube from './icons/Cube';
 import Cut from './icons/Cut';
 import Desktop from './icons/Desktop';
+import Developer from './icons/Developer';
+import Document from './icons/Document';
+import Download from './icons/Download';
 import Duplicate from './icons/Duplicate';
 import Extension from './icons/Extension';
 import Folder from './icons/Folder';
+import Forward from './icons/Forward';
 import Fullscreen from './icons/Fullscreen';
 import Gesture from './icons/Gesture';
 import Globe from './icons/Globe';
-import Grid from './icons/Grid';
 import GridAlt from './icons/GridAlt';
+import Grid from './icons/Grid';
 import Help from './icons/Help';
+import HomeFilled from './icons/HomeFilled';
+import Home from './icons/Home';
+import Import from './icons/Import';
+import InfoFilled from './icons/InfoFilled';
+import Info from './icons/Info';
+import Inspect from './icons/Inspect';
 import Italic from './icons/Italic';
 import Javascript from './icons/Javascript';
 import Key from './icons/Key';
+import Language from './icons/Language';
 import Location from './icons/Location';
+import MailFilled from './icons/MailFilled';
 import Mail from './icons/Mail';
 import Media from './icons/Media';
 import Menu from './icons/Menu';
 import Menu1 from './icons/Menu1';
+import News from './icons/News';
 import NewBookmark from './icons/NewBookmark';
 import NewFolder from './icons/NewFolder';
 import NewNote from './icons/NewNote';
-import News from './icons/News';
 import Night from './icons/Night';
 import Notepad from './icons/Notepad';
 import NumberList from './icons/NumberList';
@@ -55,19 +75,30 @@ import Pdf from './icons/Pdf';
 import Permission from './icons/Permission';
 import Phone from './icons/Phone';
 import Pip from './icons/Pip';
+import PlayFilled from './icons/PlayFilled';
 import Play from './icons/Play';
 import Preferences from './icons/Preferences';
+import PrintFilled from './icons/PrintFilled';
+import Print from './icons/Print';
 import Processes from './icons/Processes';
 import Processes1 from './icons/Processes1';
 import Qrcode from './icons/Qrcode';
 import Redo from './icons/Redo';
+import Refresh from './icons/Refresh';
 import Rss from './icons/Rss';
+import Save from './icons/Save';
 import Screenshot from './icons/Screenshot';
 import SearchSidebar from './icons/SearchSidebar';
+import Search from './icons/Search';
+import SecureFilled from './icons/SecureFilled';
+import SecureFilled12 from './icons/SecureFilled12';
+import Secure from './icons/Secure';
 import Settings from './icons/Settings';
 import ShoppingBag from './icons/ShoppingBag';
 import SidebarsLeft from './icons/SidebarsLeft';
 import SidebarsRight from './icons/SidebarsRight';
+import StarFilled from './icons/StarFilled';
+import Star from './icons/Star';
 import StarInTray from './icons/StarInTray';
 import StarInTray1 from './icons/StarInTray1';
 import StarInTray2 from './icons/StarInTray2';
@@ -75,9 +106,10 @@ import Stocks from './icons/Stocks';
 import Strikethrough from './icons/Strikethrough';
 import Subtitles from './icons/Subtitles';
 import Sun from './icons/Sun';
+import Sync from './icons/Sync';
 import Tablet from './icons/Tablet';
-import Tag from './icons/Tag';
 import TagAlt from './icons/TagAlt';
+import Tag from './icons/Tag';
 import Title1 from './icons/Title1';
 import Title2 from './icons/Title2';
 import Title3 from './icons/Title3';
@@ -85,55 +117,79 @@ import Trash from './icons/Trash';
 import Tv from './icons/Tv';
 import Underline from './icons/Underline';
 import Undo from './icons/Undo';
+import UnsecureFilled from './icons/UnsecureFilled';
+import UnsecureWarningFilled from './icons/UnsecureWarningFilled';
 import Update from './icons/Update';
+import Warning from './icons/Warning';
+import WeatherFilled from './icons/WeatherFilled';
 import Weather from './icons/Weather';
 
-const icons = [AddPrivateWindow,
-AddPrivateWindow1,
-AddWindow,
+const icons = [AddPrivate,
+AddPrivate1,
+Add,
+Add1,
 All,
-Audio,
+AppearanceFilled,
 AudioNone,
+Audio,
+AvatarFilled,
+Avatar,
+Back,
 Biscuit,
 Bold,
 BulletList,
 Calculator,
 Calculator2,
 Certificate,
+Check,
 ChevronDown,
 ChevronLeft,
 ChevronRight,
 ChevronUp,
 Clock,
-CloseTabs,
 CloseTabsToLeft,
 CloseTabsToRight,
+CloseTabs,
+Close,
 Colour,
+CompassFilled,
 Copy,
 Cube,
 Cut,
 Desktop,
+Developer,
+Document,
+Download,
 Duplicate,
 Extension,
 Folder,
+Forward,
 Fullscreen,
 Gesture,
 Globe,
-Grid,
 GridAlt,
+Grid,
 Help,
+HomeFilled,
+Home,
+Import,
+InfoFilled,
+Info,
+Inspect,
 Italic,
 Javascript,
 Key,
+Language,
 Location,
+MailFilled,
 Mail,
 Media,
 Menu,
 Menu1,
+News,
 NewBookmark,
 NewFolder,
 NewNote,
-News,
 Night,
 Notepad,
 NumberList,
@@ -145,19 +201,30 @@ Pdf,
 Permission,
 Phone,
 Pip,
+PlayFilled,
 Play,
 Preferences,
+PrintFilled,
+Print,
 Processes,
 Processes1,
 Qrcode,
 Redo,
+Refresh,
 Rss,
+Save,
 Screenshot,
 SearchSidebar,
+Search,
+SecureFilled,
+SecureFilled12,
+Secure,
 Settings,
 ShoppingBag,
 SidebarsLeft,
 SidebarsRight,
+StarFilled,
+Star,
 StarInTray,
 StarInTray1,
 StarInTray2,
@@ -165,9 +232,10 @@ Stocks,
 Strikethrough,
 Subtitles,
 Sun,
+Sync,
 Tablet,
-Tag,
 TagAlt,
+Tag,
 Title1,
 Title2,
 Title3,
@@ -175,7 +243,11 @@ Trash,
 Tv,
 Underline,
 Undo,
+UnsecureFilled,
+UnsecureWarningFilled,
 Update,
+Warning,
+WeatherFilled,
 Weather];
 
 export default icons;
